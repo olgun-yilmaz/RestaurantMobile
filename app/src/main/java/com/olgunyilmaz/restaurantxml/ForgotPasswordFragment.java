1578f6989ad2dfb088464a5ef26a6f91eae61115
olgun-yilmaz <olgunyilm@gmail.com>
2025-02-03 20:38:33 +0300

    Added forgot password fragment

---
+++ app/src/main/java/com/olgunyilmaz/restaurantxml/ForgotPasswordFragment.java
@@ -0,0 +1,32 @@
+package com.olgunyilmaz.restaurantxml;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ForgotPasswordFragment extends Fragment {
+
+
+    public ForgotPasswordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false);
+    }
+}
